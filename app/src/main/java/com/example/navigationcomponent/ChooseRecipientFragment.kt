3cc2ead09eba4650841d8bda2539f6a055780290
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-02-03 19:24:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponent/ChooseRecipientFragment.kt
@@ -0,0 +1,55 @@
+package com.example.navigationcomponent
+
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recepient.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController : NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_recepient, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next_btn -> {
+                if (!TextUtils.isEmpty(input_recipient.text.toString())){
+                    val  bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController!!.navigate(R.id.action_chooseRecepientFragment_to_specifyAmountFragment,
+                    bundle)
+                }
+
+            }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/navigationcomponent/Money.kt
@@ -0,0 +1,11 @@
+package com.example.navigationcomponent
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.math.BigDecimal
+
+@Parcelize
+data class Money(val amount : BigDecimal) : Parcelable {
+
+
+}
